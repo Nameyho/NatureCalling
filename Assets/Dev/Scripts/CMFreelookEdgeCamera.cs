e1bfb0bf4ad99e5ae89e4eccdc93cffeea6aeb7f
Oxyderkeya <bierna.sandrine@gmail.com>
2021-08-16 14:45:35 +0200

    Update Camera script

--- Assets/Dev/Scripts/CMFreelookEdgeCamera.cs
+++ Assets/Dev/Scripts/CMFreelookEdgeCamera.cs
@@ -10,36 +10,36 @@ public class CMFreelookEdgeCamera : MonoBehaviour
 	int screenWidth;
 	int screenHeight;
 
-	[Header("Zoom")]
-	[SerializeField]
-	private float _SpeedScrolling = 2f;
+	//[Header("Zoom")]
+	//[SerializeField]
+	//private float _SpeedScrolling = 2f;
 
-	[SerializeField]
-	private float _SpeedZoom = 3f;
+	//[SerializeField]
+	//private float _SpeedZoom = 3f;
 
-	[SerializeField]
-	private float ZoomInMax = 40f;
+	//[SerializeField]
+	//private float ZoomInMax = 40f;
 
-	[SerializeField]
-	private float ZoomOutMax = 90f;
+	//[SerializeField]
+	//private float ZoomOutMax = 90f;
 
-	[SerializeField]
-	private float _minScrollSpeed = 0f;
+	//[SerializeField]
+	//private float _minScrollSpeed = 0f;
 
-	[SerializeField]
-	private float _maxScrollSpeed = 200f;
+	//[SerializeField]
+	//private float _maxScrollSpeed = 200f;
 
-	private CinemachineInputProvider m_inputProvider;
-	private CinemachineFreeLook m_virtualCamera;
-	private Transform m_cameratransform;
-	private float lastFov;
+	//private CinemachineInputProvider m_inputProvider;
+	//private CinemachineFreeLook m_virtualCamera;
+	//private Transform m_cameratransform;
+	//private float lastFov;
 
-	private void Awake()
-	{
-		m_inputProvider = GetComponent<CinemachineInputProvider>();
-		m_virtualCamera = GetComponent<CinemachineFreeLook>();
+	//private void Awake()
+	//{
+	//	m_inputProvider = GetComponent<CinemachineInputProvider>();
+	//m_virtualCamera = GetComponent<CinemachineFreeLook>();
 
-	}
+	//}
 	void Start()
 	{
 		CinemachineCore.GetInputAxis = GetAxisCustom;
@@ -48,35 +48,36 @@ public class CMFreelookEdgeCamera : MonoBehaviour
 	}
 	private void Update()
 	{
-		float z = m_inputProvider.GetAxisValue(2);
+		//	float z = m_inputProvider.GetAxisValue(2);
 		Cursor.lockState = CursorLockMode.Confined;
 
-		if (z != 0)
-		{
-			ZoomScreen(z);
-		}
+		//	if (z != 0)
+		//	{
+		//		ZoomScreen(z);
+		//	}
 
 	}
 
-	public void ZoomScreen(float increment)
-	{
-		float fov = m_virtualCamera.m_Lens.FieldOfView;
-		float target = Mathf.Clamp(fov + increment, ZoomOutMax, ZoomInMax);
-		m_virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(fov, target, _SpeedZoom * Time.deltaTime);
+	//public void ZoomScreen(float increment)
+	//{
+	//	float fov = m_virtualCamera.m_Lens.FieldOfView;
+	//	float target = Mathf.Clamp(fov + increment, ZoomOutMax, ZoomInMax);
+	//	m_virtualCamera.m_Lens.FieldOfView = Mathf.Lerp(fov, target, _SpeedZoom * Time.deltaTime);
 
-		if (lastFov < fov && (ZoomOutMax >= fov))
-		{
-			_SpeedScrolling += (_SpeedScrolling / (ZoomOutMax - ZoomInMax) * fov);
-		}
+	//	if (lastFov < fov && (ZoomOutMax >= fov))
+	//	{
+	//		_SpeedScrolling += (_SpeedScrolling / (ZoomOutMax - ZoomInMax) * fov);
+	//	}
 
-		else if (lastFov > fov && (ZoomInMax <= fov))
-		{
-			_SpeedScrolling -= (_SpeedScrolling / (ZoomOutMax - ZoomInMax) * fov);
-		}
-		lastFov = fov;
-		_SpeedScrolling = Mathf.Clamp(_SpeedScrolling, _minScrollSpeed, _maxScrollSpeed);
+	//	else if (lastFov > fov && (ZoomInMax <= fov))
+	//	{
+	//		_SpeedScrolling -= (_SpeedScrolling / (ZoomOutMax - ZoomInMax) * fov);
+	//	}
+	//	lastFov = fov;
+	//	_SpeedScrolling = Mathf.Clamp(_SpeedScrolling, _minScrollSpeed, _maxScrollSpeed);
 
-	}
+	//}
+
 	public float GetAxisCustom(string axisName)
 	{
 		if (axisName == "Mouse X")
@@ -89,17 +90,22 @@ public class CMFreelookEdgeCamera : MonoBehaviour
 			{
 				return -speed;
 			}
-		}
-		else if (axisName == "Mouse Y")
-		{
-			if (Input.mousePosition.y > screenHeight - boundary)
+			//} else if (axisName == "Mouse ScrollWheel")
+			//	{
+			//		return Input.GetAxis(axisName);
+			//	}
+			else if (axisName == "Mouse Y")
 			{
-				return speed;
+				if (Input.mousePosition.y > screenHeight - boundary)
+				{
+					return speed;
+				}
+				if (Input.mousePosition.y < boundary)
+				{
+					return -speed;
+				}
 			}
-			if (Input.mousePosition.y < boundary)
-			{
-				return -speed;
-			}
+			return 0;
 		}
 		return 0;
 	}
