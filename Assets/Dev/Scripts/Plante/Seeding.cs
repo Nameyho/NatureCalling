6b28ee05ee2a9b2f4fbb67339bee90537a9744c0
Nameyho <mathieuallard1993@hotmail.com>
2021-09-23 17:18:54 +0200

    [origin/dev] fix basket

--- Assets/Dev/Scripts/Plante/Seeding.cs
+++ Assets/Dev/Scripts/Plante/Seeding.cs
@@ -260,13 +260,14 @@ public class Seeding : MonoBehaviour
                 }
                     if (!cs._isAquaticPlant && !cs._isPlant && !cs._isBuilding &&
                     !cs._isShovel && !cs._isLayering  && !cs._isBugHostel && 
-                    !cs._isHenHouse && !cs._isInsectPollinator &&  !cs._isHen &&
+                    !cs._isHenHouse && !cs._isInsectPollinator &&  !cs._isHen && !cs._IsBasket &&
                     hit.transform.tag == "BuildingZone")
                     {
                         GameObject go = Instantiate(_plantsPrefabs, hit.point, Quaternion.identity);
                         go.transform.Rotate(0, _DaD.GetRotationY(), 0);
                         GameObject vfx = Instantiate(_GoodVFX, hit.point, Quaternion.identity);
                         Destroy(vfx, 5f);
+                    
                         _remainingCards.Value--;
                         _gameManager.AddProgression(cs._bonusBioDiversity);
 
@@ -278,6 +279,7 @@ public class Seeding : MonoBehaviour
                     Destroy(vfx, 5f);
                 }
 
+                
 
                 //if (cs._isWaterCan && hit.transform.tag == "Plants")
                 //{
